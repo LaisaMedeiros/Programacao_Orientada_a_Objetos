51984d81ce6764049ff9875c172793886a24faf1
Laísa Medeiros <111201414+LaisaMedeiros@users.noreply.github.com>
2025-09-24 15:47:21 -0300

    Create Teste.java

---
+++ SuperClasse/Pessoa/Teste.java
@@ -0,0 +1,6 @@
+public class Teste {
+    public static void main(String[] args) {
+        Estudante e = new Estudante("Laísa", 1234);
+        System.out.println(e.nome + " - " + e.matricula);
+    }
+}
