9431b2bbebd7bb50afcd9c0e80254b1deb0bb039
Laísa Medeiros <111201414+LaisaMedeiros@users.noreply.github.com>
2025-09-24 16:29:43 -0300

    Update Individuo.java

--- SuperClasse/Individuo/Individuo.java
+++ SuperClasse/Individuo/Individuo.java
@@ -3,6 +3,6 @@ class Individuo{
   int idade; 
 
   public void apresentar(){
-    System.out.println("Olá, meu nome é", this.nome, "e tenho", this.idade, "anos");
+    System.out.println("Olá, meu nome é" + this.nome + "e tenho" + this.idade + "anos");
   }
 }
