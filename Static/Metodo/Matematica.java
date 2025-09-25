83edf7095f7a1817bccbbd20ac4eca89705124f2
Laísa Medeiros <111201414+LaisaMedeiros@users.noreply.github.com>
2025-09-25 09:03:30 -0300

    Create Matematica.java

---
+++ Static/Metodo/Matematica.java
@@ -0,0 +1,13 @@
+public class Matematica {
+	public static int somar (int numeroA, int numeroB) {
+		return numeroA + numeroB;
+	}
+	
+	public static double calcularAreaQuadrado (double lado){
+		return lado +lado;
+	}
+	
+	public static double calcularAreaCirculo(double raio) {
+		return 3.14*raio*raio; 
+	}
+}
