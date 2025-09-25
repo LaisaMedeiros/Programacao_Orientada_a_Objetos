8a09ec08cfe1cc8e9645bb90dc0b74497b1e8376
Laísa Medeiros <111201414+LaisaMedeiros@users.noreply.github.com>
2025-09-25 10:21:18 -0300

    Create Gato.java

---
+++ SuperClasse/Animal/Gato.java
@@ -0,0 +1,15 @@
+class Gato extends Animal {
+    private String nome;
+
+    public void comer(){
+       System.out.println("O gato comeu!"); 
+    }
+
+    public void beber(){
+        System.out.println("O gato bebeu!"); 
+    }
+
+    public void miar(){
+        System.out.println("O gato maiou!"); 
+    }
+}
