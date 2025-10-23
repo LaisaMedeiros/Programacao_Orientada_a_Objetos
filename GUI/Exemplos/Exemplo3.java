b1191c6a784a9b7d3b51190f5b34a8d7aa68f1df
Laísa Medeiros <111201414+LaisaMedeiros@users.noreply.github.com>
2025-10-23 10:16:13 -0300

    Create Exemplo3.java

---
+++ GUI/Exemplos/Exemplo3.java
@@ -0,0 +1,24 @@
+// Exemplo 3
+import javax.swing.*;
+import java.awt.*;
+
+public class GridLayoutExemplo {
+    public static void main(String[] args) {
+		
+        JFrame janela = new JFrame("Exemplo GridLayout");
+        janela.setSize(300, 200);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // 3 linhas, 2 colunas, espaçamento horizontal e vertical de 5 pixels
+        janela.setLayout(new GridLayout(3, 2, 5, 5));
+
+        // Adicionando botões
+        janela.add(new JButton("Botão 1"));
+        janela.add(new JButton("Botão 2"));
+        janela.add(new JButton("Botão 3"));
+        janela.add(new JButton("Botão 4"));
+        janela.add(new JButton("Botão 5"));
+        janela.add(new JButton("Botão 6"));
+
+        janela.setVisible(true);
+	}
+}
